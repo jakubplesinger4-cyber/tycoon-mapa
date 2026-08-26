8b7cbada77a0ee9fa1d48f91b85a7c44ef06bc6e
jakubplesinger4-cyber <jakubplesinger4@gmail.com>
2026-08-26 16:36:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,21 @@
-// Název tvého souboru
-const PMTILES_URL = "moje-mapa.pmtiles";
+// 1. Zadej přesný název tvého pmtiles souboru
+const PMTILES_URL = "moje-mapa_2.pmtiles";
 
-// 1. Zaregistrujeme formát PMTiles do mapového enginu
+// 2. Zaregistrujeme formát PMTiles do mapového enginu
 const protocol = new pmtiles.Protocol();
 maplibregl.addProtocol("pmtiles", protocol.tile);
 
-// 2. Stáhneme oficiální "světlý" vizuální styl pro Protomaps
-fetch("https://protomaps.github.io/basemaps-assets/style-light.json")
-  .then(response => response.json())
-  .then(style => {
-    
-    // 3. TADY JE TO KOUZLO: Vnutíme stylu tvůj lokální 13MB soubor místo stahování z internetu
-    style.sources.protomaps.url = `pmtiles://${PMTILES_URL}`;
+// 3. Vygenerujeme styl přímo v kódu (světlé téma "light")
+const myStyle = protomaps_themes_base.default(`pmtiles://${PMTILES_URL}`, "light");
 
-    // 4. Inicializace samotné mapy
-    const map = new maplibregl.Map({
-      container: "map",
-      style: style,          // Napojení upraveného stylu
-      center: [16.55, 49.1], // POZOR! MapLibre má souřadnice opačně než Leaflet: [délka, šířka]
-      zoom: 12
-    });
-    
-    // Přidání navigačních tlačítek (zoom) do rohu
-    map.addControl(new maplibregl.NavigationControl());
-  });
+// 4. Inicializace samotné mapy
+const map = new maplibregl.Map({
+  container: "map",
+  style: myStyle,
+  // POZOR: Zde musíš mít souřadnice, které leží uvnitř tvého výřezu! [délka, šířka]
+  center: [16.55, 49.1], 
+  zoom: 12
+});
+
+// 5. Přidání navigačních tlačítek (+/-)
+map.addControl(new maplibregl.NavigationControl());
